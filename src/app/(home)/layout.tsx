26c50227de41b030734b1e473a881be995c64c00
Hassen Taidirt <hi@hassen.io>
2023-10-04 23:28:15 +0200

    feat: Add background gradient

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -6,9 +6,9 @@ export default function HomeLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
+    <div className="w-screen min-h-screen bg-gradient-to-br from-gray-100 to-blue-100">
         <Navbar />
         <main>{children}</main>
-    </>
+    </div>
   )
 }
